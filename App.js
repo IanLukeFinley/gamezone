10322f180a25ab2fb90a317c55e8f4474ff29061
Ian Finley <ianlukefinley@gmail.com>
2023-02-14 11:08:54 -0500

    Lesson 17 - Importing Fonts

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './screens/home';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/Fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/Fonts/Nunito-SemiBold.ttf')
+});
 
 export default function App() {
-  return (
-   <Home />
-  );
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Home />
+     );
+  } else {
+    return (
+      //<Home
+      <AppLoading 
+      startAsync={getFonts}
+      onFinish={() => setFontsLoaded(true)}
+      onError = {console.warn}
+      />
+    )
+  }
 }
