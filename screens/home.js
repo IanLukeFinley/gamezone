3df36ec914d6ea54f81ddcee729406a43dae468c
Ian Finley <ianlukefinley@gmail.com>
2023-02-15 09:44:52 -0500

    Lesson 22 - Passing Params

--- screens/home.js
+++ screens/home.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import {globalStyles} from '../styles/global';
 
 export default function Home ({ navigation }) {
 
-    const pressHandler = () => {
-        navigation.navigate('Review Details')
-    }
+    const [reviews, setReviews ] = useState([
+        {title: 'Zelda, Breath of the Wild', rating: 5, body: 'text...', key: 1 },
+        {title: 'Pokemon Silver', rating: 4, body: 'text 2...', key: 2 },
+        {title: 'Final Fantasy II', rating: 3, body: 'text3...', key: 3 },
+    ])
 
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button onPress={pressHandler} title="Go to Details"/>
+            <FlatList 
+                data={reviews}
+                renderItem={({item}) => (
+                    <TouchableOpacity onPress={() => {navigation.navigate('Review Details', item)}}>
+                        <Text style={globalStyles.title}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import {globalStyles} from '../styles/global';
 
-export default function ReviewDetails () {
+export default function ReviewDetails ({ route, navigation }) {
+
+    const {title, rating, body, key} = route.params;
 
     return (
         <View style={globalStyles.container}>
-            <Text>Review Screen</Text>
+            <Text>Title: {JSON.stringify(title)}</Text>
+            <Text>Rating: {JSON.stringify(rating)}</Text>
+            <Text>Review: {JSON.stringify(body)}</Text>
         </View>
     )
 }
